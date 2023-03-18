d6ad8f26d792a2e4a88f7e41ac26cd02ec675c70
ASDASD SDQASD <elicei3322@gmail.com>
2023-03-18 19:31:49 +0300

    формула для рандомки, недоделанная функция по умножению нецелых чисел.

--- OpenGL1/main.cpp
+++ OpenGL1/main.cpp
@@ -3,19 +3,52 @@
 #include <iostream>
 
 
+
 void test() {
-    float *x1 = new float(1.0f);
-    float *x2 = new float(1.0f);
-    std::cout << "res float exex: " << *x1 * *x2 << std::endl;
+    setlocale(LC_ALL, "Rus");
+    float* x1 = new float(0.9f);
+    //float *x2 = new float(0.9f);
+    float x3 = 1;
+    for (; x3 > 0;) {
+        x3 = *x1 * *x1;
+        
+        std::cout << "Умножение 1.0f и 1.0f: " << x3 << std::endl;
+    }
+    delete x1;
+    //Создание динамического массива:
+    int N;
+    std::cout << "Размер массива: " << std::endl;
+    std::cin >> N;
+    std::cout << "Предельные значения элементов?: \n" << "Максимальный: ";
+    short r1; std::cin >> r1;
+    std::cout << "\nМинимальный: ";
+    short r2; std::cin >> r2;
+    if (r1 < r2) std::cout << "Ошибка";
+    else {
+        std::cout << "\n";
+        int* arr = new int[N];
+        short i = 0;
+        srand(time(NULL));
+        for (; i < N; i++)
+        {
+            arr[i] = rand() %(r1 - r2 + 1) + r2; // rand()%(b-a+1) + a; a - левая b - правая граница
+        }
+        for (i = 0; i < N; i++)
+            std::cout << "arr[" << i << "] :" << arr[i] << std::endl;
+        delete[] arr;
+        }
+    test();
 }
 
-int main(void)
+int main()
 {
+    //std::cout << glfwGetPrimaryMonitor(); куча нулей 
     test();
+    GLFWmonitor* primary = glfwGetPrimaryMonitor();    
     float* theta = new float(0.0f);
     float* move = new float(0.0f);
     GLFWwindow* window;
-
+    //почему то если создавать и объявлять здесь параметры окна, то не работает
     /* Initialize the library */
     if (
         !glfwInit()
@@ -25,10 +58,11 @@ int main(void)
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow
     (
-        640,
-        480,
+        1920,
+        1080,
         "Hello, Triangle",
-        NULL, NULL
+        glfwGetPrimaryMonitor(), /*glfwGetPrimaryMonitor() - во весь экран(основной монитор)*/
+        NULL /*Для работы с несколькими окнами(ВЗАИМОДЕЙСТВИЯ нескольких окон)*/
     );
 
     if (!window)
@@ -47,7 +81,7 @@ int main(void)
         1920 / 2 - 320,
         1080 / 2 - 240
     );
-
+    short count = 0;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -58,8 +92,8 @@ int main(void)
         
         glBegin(GL_TRIANGLES);
         glColor3f(1.0f, 0.0f, 1.0f); glVertex2f(0.5f, -0.7f);
-        glColor3f(0.0f, 1.0f, 0.0f);      glVertex2f(0.0f, 0.5f);
-        glColor3f(0.0f, 0.0f, 1.0f);                  glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f); glVertex2f(0.0f, 0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f); glVertex2f(-0.5f, -0.5f);
         glEnd();
         
 
@@ -76,16 +110,20 @@ int main(void)
 
         glPopMatrix();
 
-        *theta -= 0.5f;
-        *move -= 0.5f;
+        *theta += 0.5f;
+        *move  += 0.5f;
+        /**theta *= 0.5f;
+        *move *= 0.5f;  после первого же прогона до 0 округляется и сё*/
         Sleep(1);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
-
+    
     /* Poll for and process events */
     glfwPollEvents();//пока не завершится предыдущее событие, не будет "отвечать"
-}
+    }
+    std::cout << *theta << "\n";
+    std::cout << *move << "\n";
     delete theta;
     delete move;
     glfwDestroyWindow(window);
